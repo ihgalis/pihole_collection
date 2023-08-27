950ef1e0eb604ec4ab2ee9fe2e44b635f5d659cc
Andre Fritsche <andre.fritsche@outlook.de>
2023-08-27 23:03:55 +0200

    Update main.py
    Automated DNS Server choice + more timeout time

--- dns_checker /main.py
+++ dns_checker /main.py
@@ -4,7 +4,7 @@ import dns.resolver
 import datetime
 import time
 
-from random import randint
+from random import randint, choice
 
 class Colors:
     GREEN = '\033[92m'
@@ -22,19 +22,48 @@ def check_domains(domain_list, dns_server=None):
     alive_domains = []
     dead_domains = []
 
+    # List of known public DNS servers
+    public_dns_servers = [
+        '8.8.8.8',          # Google DNS
+        '8.8.4.4',          # Google DNS
+        '1.1.1.1',          # Cloudflare DNS
+        '1.0.0.1',          # Cloudflare DNS
+        '208.67.222.222',   # OpenDNS
+        '208.67.220.220',   # OpenDNS
+        '9.9.9.9',          # Quad9
+        '149.112.112.112',  # Quad9
+        '64.6.64.6',        # Verisign
+        '76.76.2.0',        # Control D
+        '76.76.10.0',       # Control D
+        '94.140.14.14',     # AdGuard DNS
+        '94.140.15.15',     # AdGuard DNS
+        '8.26.56.26',       # Comodo Secure DNS
+        '8.20.247.20'       # Comodo Secure DNS
+    ]
+
     resolver = dns.resolver.Resolver()
-    if dns_server:
-        resolver.nameservers = [dns_server]
+    resolver.timeout = 10
+    resolver.lifetime = 10
 
     for domain in domain_list:
         print_with_timestamp(f"Reading domain: {domain}")
         temp_waittime = randint(2, 7)
         print_with_timestamp(f"Waiting {temp_waittime}")
         time.sleep(temp_waittime)
+        
+        # Choose a random DNS server from the list if none is provided
+        if not dns_server:
+            dns_server = choice(public_dns_servers)
+        
+        print_with_timestamp(f"Using DNS server: {dns_server}")
+            
+        resolver.nameservers = [dns_server]
         try:
             resolver.resolve(domain, 'A')  # Try to retrieve the A record of the domain
             alive_domains.append(domain)
             print_with_timestamp(f"Domain {domain} is alive.", Colors.GREEN)
+        except dns.exception.Timeout:
+            print_with_timestamp(f"Timeout while querying domain {domain}.", Colors.RED)
         except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
             dead_domains.append(domain)
             print_with_timestamp(f"Domain {domain} is dead.", Colors.RED)
@@ -44,7 +73,7 @@ def check_domains(domain_list, dns_server=None):
 def main():
     parser = argparse.ArgumentParser(description="Check if domains are resolvable by a specified DNS server.")
     parser.add_argument("--input", required=True, help="Path to the input file containing domains.")
-    parser.add_argument("--dns", default=None, help="IP address of the DNS server to use for checks.")
+    parser.add_argument("--dns", default=None, help="IP address of the DNS server to use for checks. If not provided, a random public DNS server will be chosen.")
     parser.add_argument("--alive", required=True, help="Path to the output file for alive domains.")
     parser.add_argument("--dead", required=True, help="Path to the output file for dead domains.")
     args = parser.parse_args()
